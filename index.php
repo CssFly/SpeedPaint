4bd55ec18d7e9bf81fcd723daad6ec7a22158b48
CssFly <ph73@gmx.de>
2015-05-25 20:49:39 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,61 @@
+
+<?php ?>
+<!doctype html>
+<html lang="en">
+
+<head>
+<title>Create Speed Paintings online. Just draw and click Play.</title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="robots" content="index,follow" />
+<meta name="description" content="Use our free online drawing tools to create animated speed paint drawings and show off your artwork in the public gallery." />
+<meta name="revisit-after" content="7 days" />
+
+<style type="text/css">
+
+body,html{ padding:0; margin:0; height:100%; overflow-x:hidden; line-height: 1em; font-size: 1em; font-family: verdana; }
+
+.teaser {
+  display: table;
+  height: 5%;
+  line-height: 1em;
+  padding: 0;
+  text-align: center;
+  width: 100%;
+  overflow: hidden;
+}
+.teaser a {
+  background: #fff none repeat scroll 0 0;
+  color: #00f;
+  display: table-cell;
+  text-align: center;
+  text-decoration: underline;
+  vertical-align: middle;
+  width: 50%;
+}
+.teaser a .s2 { display: none; }
+
+.teaser a:hover,
+.teaser a:active {
+  background: #fff;
+  color: #000;
+  text-decoration: none;
+}
+.teaser a:hover .s2,
+.teaser a:active .s2 {
+  display: inline-block;
+}
+.teaser a:hover .s1,
+.teaser a:active .s1 {
+  display: none;
+}
+</style>
+</head>
+
+<body>
+
+  <iframe scrolling="no" width="100%" height="95%" frameborder="0" border="0" src="http://www.queeky.com/app?apikey=b689fb2d0d58900c310ce4edd1e10a13&amp;backgroundColor=000000"></iframe>
+  <div class="teaser" title="Learn more about QueekyPaint API"><a href="/queeky/gallery.php">Browse SpeedPaint Gallery</a><a href="http://www.queeky.com/api"><span class="s1">SpeedPaint.info is build on QueekyPaint API</span><span class="s2">Learn more about QueekyPaint API ...</span></a></div>
+
+</body>
+</html>
